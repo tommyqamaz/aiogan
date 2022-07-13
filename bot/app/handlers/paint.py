bcd99c4f5d16f016ddc25b5eb987b5ea71d500c6
qamaz <solgrini@yandex.ru>
2022-07-13 12:01:58 +0300

    minor changes

--- bot/app/handlers/paint.py
+++ bot/app/handlers/paint.py
@@ -5,7 +5,7 @@ import logging
 import threading
 from io import BytesIO
 
-from painting_mode.test import *
+from painting_mode.inference import CycleGAN
 
 from aiogram.dispatcher import FSMContext
 from aiogram.types.message import ContentType
@@ -19,9 +19,9 @@ from app.config_reader import load_config
 available_artists = ["van Gogh", "Cezanne", "Hulio Perdulio"]
 
 
-class GNSTStates(StatesGroup):
+class GANstyle(StatesGroup):
     """
-    Generative Neural Style Transfer States
+    CycleGAN states group
     """
 
     choose_painter = State()
@@ -31,7 +31,7 @@ class GNSTStates(StatesGroup):
 
 async def cmd_paint(message: Message):
     """
-    Enables painting mode
+    Paint entry point
     """
     await message.answer(":)")
     logging.info("cmd_paint")
@@ -39,11 +39,13 @@ async def cmd_paint(message: Message):
     for painter in available_artists:
         keyboard.add(painter)
     await message.answer("Choose a painter for style transfer:", reply_markup=keyboard)
-    await GNSTStates.choose_painter.set()
+    await GANstyle.choose_painter.set()
 
 
 async def cmd_choose_painter(message: Message, state: FSMContext):
-    """Choose painter for further transfer"""
+    """
+    Choose painter for further transfer
+    """
     if message.text not in available_artists:
         await message.answer("Choose a painter via keyboard below")
     logging.info("cmd_choose_painter")
@@ -52,12 +54,12 @@ async def cmd_choose_painter(message: Message, state: FSMContext):
     await message.answer(
         "Upload a content image...", reply_markup=ReplyKeyboardRemove()
     )
-    await GNSTStates.upload_content.set()
+    await GANstyle.upload_content.set()
 
 
 async def cmd_upload_content(message: Message, state: FSMContext):
     """
-    Downloads content image and executes GNST in another thread
+    Downloads content image and starts CycleGAN in thread
     """
     # Check if the message contains a photo
     if not message.photo:
@@ -71,7 +73,7 @@ async def cmd_upload_content(message: Message, state: FSMContext):
     # Prepare to run GNST
     logging.info("Painting — running CycleGAN...")
     await message.answer("Recieved!")
-    await GNSTStates.run_gnst.set()
+    await GANstyle.run_gnst.set()
     await message.answer("Wait a bit...")
     # Signal typing
     await ChatActions.typing()
@@ -87,14 +89,14 @@ async def cmd_upload_content(message: Message, state: FSMContext):
 
 async def run_cyclegan(message: Message, state: FSMContext, content_path):
     """
-    Runs Generative Neural Style Transfer with CycleGAN
+    Functions for CycleGAN inference
     """
     users_chose = await state.get_data()
 
     # Instantiate GNST class
-    gnst = GNST(content_path, users_chose["choose_painter"])
+    cycleGAN = CycleGAN(content_path, users_chose["choose_painter"])
     # Run GNST
-    painted_image, time_passed = gnst.transfer_style()
+    painted_image, time_passed = cycleGAN.transfer_style()
     # Send stylized image
     bytes = BytesIO()
     bytes.name = "painted_image.jpg"
@@ -105,12 +107,12 @@ async def run_cyclegan(message: Message, state: FSMContext, content_path):
     await bot_gnst.send_photo(
         message.chat.id,
         photo=bytes,
-        caption=f"Stylization took only {time_passed} seconds ⏰",
+        caption=f"Stylization took only {time_passed} seconds!",
     )
     await bot_gnst.close()
     # Clear memory storage
     os.remove(content_path)
-    del gnst
+    del cycleGAN
     gc.collect()
     # End GNST state
     logging.info("Stylization — GNST completed!")
@@ -119,9 +121,9 @@ async def run_cyclegan(message: Message, state: FSMContext, content_path):
 
 def register_handlers_paint(dp: Dispatcher):
     dp.register_message_handler(cmd_paint, commands="paint", state="*")
-    dp.register_message_handler(cmd_choose_painter, state=GNSTStates.choose_painter)
+    dp.register_message_handler(cmd_choose_painter, state=GANstyle.choose_painter)
     dp.register_message_handler(
         cmd_upload_content,
-        state=GNSTStates.upload_content,
+        state=GANstyle.upload_content,
         content_types=ContentType.ANY,
     )
